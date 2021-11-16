482d673b7f796eb8dcd4f0e001ada165d30ed261
maithevien9 <maithevien9@gmail.com>
2021-11-16 14:56:48 +0700

    Init project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login';
 
 function App() {
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+
+        <GoogleLogin
+          clientId='514660113082-vcngk7hvilmjgca3eti7vsha1efldk5v.apps.googleusercontent.com'
+          buttonText='Login'
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
       </header>
     </div>
   );
