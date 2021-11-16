de7e1ed175623757a1e1e95b63a51ce98afdf1e8
maithevien9 <maithevien9@gmail.com>
2021-11-16 15:29:38 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import GoogleLogin from 'react-google-login';
+import React, { useState } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import axios from 'axios';
 
+const clientId = '411768487503-e06gsoh9etobrarghoagn8gbh6fjo7u8.apps.googleusercontent.com';
 function App() {
-  const responseGoogle = (response) => {
-    console.log(response);
+  const [showloginButton, setShowloginButton] = useState(true);
+  const [showlogoutButton, setShowlogoutButton] = useState(false);
+  const onLoginSuccess = async (response) => {
+    const res = await axios.post('http://localhost:8000/auth/google', {
+      token: response.tokenId,
+    });
+
+    if (res.data) {
+      console.log(res.data.email);
+      setShowloginButton(false);
+      setShowlogoutButton(true);
+    }
+  };
+
+  const onLoginFailure = (res) => {
+    console.log('Login Failed:', res);
+  };
+
+  const onSignoutSuccess = () => {
+    alert('You have been logged out successfully');
+    console.clear();
+    setShowloginButton(true);
+    setShowlogoutButton(false);
   };
 
   return (
@@ -12,13 +36,22 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
 
-        <GoogleLogin
-          clientId='514660113082-vcngk7hvilmjgca3eti7vsha1efldk5v.apps.googleusercontent.com'
-          buttonText='Login'
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy={'single_host_origin'}
-        />
+        <div>
+          {showloginButton ? (
+            <GoogleLogin
+              clientId={clientId}
+              buttonText='Sign In'
+              onSuccess={onLoginSuccess}
+              onFailure={onLoginFailure}
+              cookiePolicy={'single_host_origin'}
+              isSignedIn={true}
+            />
+          ) : null}
+
+          {showlogoutButton ? (
+            <GoogleLogout clientId={clientId} buttonText='Sign Out' onLogoutSuccess={onSignoutSuccess}></GoogleLogout>
+          ) : null}
+        </div>
       </header>
     </div>
   );
